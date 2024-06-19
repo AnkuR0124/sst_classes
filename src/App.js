4ba09c3a398fcf5fcf41d5faea5dea1931e80e24
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2024-06-19 10:45:54 +0530

    sst classes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { a, b } from './components/Products/Products';
+import Products from './components/Products/Products';
+function App() {
+  // state variable
+  // inc
+  // dec
+  console.log(a, b);
+  return (
+    <div className="App">
+      <Products />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/AddToCart/AddToCart.js
@@ -0,0 +1,15 @@
+function AddToCart({inputV}) {
+    return (
+        <div>
+            AddToCart  - {inputV} 
+        </div>
+ )
+}
+
+export default AddToCart;
+
+
+// array of object
+// object of object
+// [{id:1, quatity: 10}, {id:2, quatity:10}, {id:3, quatity}]
+// cart
